53456ea999f139baf25577e4b75f32200a456e45
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-08-11 02:01:18 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue";
+console.log(TheWelcome)
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
